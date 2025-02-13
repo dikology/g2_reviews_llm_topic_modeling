91e53b4fda40cd15c11fea743be099192522ed9a
dikology <dgimaletdinov@gmail.com>
2025-02-13 18:48:39 +0300

    backup streamlit

--- src/extract_topic.py
+++ src/extract_topic.py
@@ -108,9 +108,9 @@ def summarize_sequential(top_n_cluster):
         data and cluster labels.
     """
     # Limit top_n_cluster to 1 for testing purposes
-    top_n_cluster = dict(
-        list(top_n_cluster.items())[:10]
-    )  # Keep only 10 clusters for testing
+    # top_n_cluster = dict(
+    #     list(top_n_cluster.items())[:1]
+    # )
 
     # Creating a progress bar
     progress_bar = st.progress(0)
